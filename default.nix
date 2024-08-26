dbdd0ee15d7d045682a476cb97782a98f04a6244
luckycyang <kfove.liang@protonmail.com>
2024-08-26 20:19:26 +0800

    add form

--- default.nix
+++ default.nix
@@ -5,20 +5,20 @@
 # Having pkgs default to <nixpkgs> is fine though, and it lets you use short
 # commands such as:
 #     nix-build -A mypackage
-{pkgs ? import <nixpkgs> {}}: {
+{ pkgs ? import <nixpkgs> { } }: {
   # The `lib`, `modules`, and `overlays` names are special
-  lib = import ./lib {inherit pkgs;}; # functions
+  lib = import ./lib { inherit pkgs; }; # functions
   modules = import ./modules; # NixOS modules
   overlays = import ./overlays; # nixpkgs overlays
 
-  example-package = pkgs.callPackage ./pkgs/example-package {};
-  gear = pkgs.callPackage ./pkgs/gear {};
-  wemeet = pkgs.callPackage ./pkgs/wemeet {};
-  yosys-synlig = pkgs.callPackage ./pkgs/synlig {};
-  probe-rs = pkgs.callPackage ./pkgs/probe-rs {};
-  flutter_rust_bridge_codegen = pkgs.callPackage ./pkgs/flutter_rust_bridge_codegen {};
-  dioxus-cli = pkgs.callPackage ./pkgs/dioxus-cli {};
-  zen = (pkgs.callPackage ./pkgs/zen-kernels {}).zen;
+  example-package = pkgs.callPackage ./pkgs/example-package { };
+  gear = pkgs.callPackage ./pkgs/gear { };
+  wemeet = pkgs.callPackage ./pkgs/wemeet { };
+  yosys-synlig = pkgs.callPackage ./pkgs/synlig { };
+  probe-rs = pkgs.callPackage ./pkgs/probe-rs { };
+  flutter_rust_bridge_codegen = pkgs.callPackage ./pkgs/flutter_rust_bridge_codegen { };
+  dioxus-cli = pkgs.callPackage ./pkgs/dioxus-cli { };
+  form = pkgs.callPackage ./pkgs/form { };
   # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
   # ...
 }
